d8e87bf9f5f84e788e54b03a0b9ea471c725d305
Vicen621 <71749981+vicen621@users.noreply.github.com>
2024-12-31 20:20:49 -0300

    created method generate_pseudo_legal_king_moves

--- src/generators/king_moves.rs
+++ src/generators/king_moves.rs
@@ -1,4 +1,4 @@
-use crate::{bitboard::{BitBoard, BitBoardMethods, EMPTY}, chess_move::Direction, defs::Square};
+use crate::{bitboard::{BitBoard, BitBoardMethods, EMPTY}, chess_move::{Direction, Move, MoveExtractor, MoveType}, defs::Square, pieces::PieceType, position::Position};
 
 struct KingLookup {
     moves: [BitBoard; 64],
@@ -16,12 +16,11 @@ impl KingLookup {
         Self { moves }
     }
 
-    pub fn get_king_moves(&self, square: Square) -> BitBoard {
-        self.moves[square.to_index()]
-    }
+    pub fn generate_pseudo_legal_king_moves(&self, position: &Position, moves: &mut Vec<Move>) {
+        let king = position.get_pieces_color_type(position.get_side_to_move(), PieceType::King);
 
-    pub fn get_king_moves_bb(&self, bb: BitBoard) -> BitBoard {
-        self.moves[bb.trailing_zeros() as usize]
+        self.generate_king_attacks(position, moves, king);
+        self.generate_king_quiet_moves(position, moves, king);
     }
 }
 
@@ -36,10 +35,34 @@ impl KingLookup {
 
         attacks
     }
+
+    fn generate_king_quiet_moves(&self, position: &Position, moves: &mut Vec<Move>, king: BitBoard) {
+        let empty_squares = !position.get_all_pieces();
+
+        let quiet_moves = self.get_king_moves_bb(king) & empty_squares;
+
+        MoveExtractor::extract_moves(king.to_square(), quiet_moves, moves, MoveType::Quiet);
+    }
+
+    fn generate_king_attacks(&self, position: &Position, moves: &mut Vec<Move>, king: BitBoard) {
+        let color = position.get_side_to_move();
+        let their_king = position.get_pieces_color_type(!color, PieceType::King);
+        let captures = position.get_pieces_color(!color) & !their_king;
+
+        let king_captures = self.get_king_moves_bb(king) & captures;
+
+        MoveExtractor::extract_moves(king.to_square(), king_captures, moves, MoveType::Quiet);
+    }
+
+    fn get_king_moves_bb(&self, bb: BitBoard) -> BitBoard {
+        self.moves[bb.trailing_zeros() as usize]
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+
     use super::*;
 
     #[test]
@@ -62,12 +85,17 @@ mod tests {
     }
 
     #[test]
-    fn test_king_lookup_square() {
+    fn test_generate_pseudo_legal_king_moves() {
+        let mut moves = vec![];
+        let position = Position::from_str("8/8/8/8/4R3/4K3/4r3/8 w - - 0 1").unwrap();
         let lookup = KingLookup::new();
-        let square = Square::A8;
-        let moves = lookup.get_king_moves(square);
-        let expected = BitBoard::from_square(Square::B7) | BitBoard::from_square(Square::A7) | BitBoard::from_square(Square::B8);
 
-        assert_eq!(moves, expected);
+        lookup.generate_pseudo_legal_king_moves(&position, &mut moves);
+
+        moves.iter().for_each(|m| -> () {
+            println!("{}", m.to_uci());
+        });
+
+        assert_eq!(moves.len(), 7);
     }
 }
